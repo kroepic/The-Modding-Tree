f73435182a80a621ea8d6121dcfc2a6e2c357bc4
kroepic <121599397+kroepic@users.noreply.github.com>
2023-03-14 10:00:06 +0800

    Updated ModINFO

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The WEIRD Tree",
+	id: "kroepic69",
+	author: "kroepic",
+	pointsName: "weird",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
